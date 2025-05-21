308a00944f26d8f442b64169b48a1d9011bf52d5
SrVitinho <55586232+SrVitinho@users.noreply.github.com>
2025-05-21 00:02:54 -0300

    Add: Photos storage control and return

--- Item/itemBase.py
+++ Item/itemBase.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 
 class ItemBase(BaseModel):
-    ID: int
     Nome: str
     Descricao: str
     Categoria: str
--- Item/itemController.py
+++ Item/itemController.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter, UploadFile, Form
+from fastapi.params import File
+from fastapi.responses import FileResponse
 from typing import Annotated
 from sqlalchemy.orm import Session
 from User.userBase import UserBase, UserResponse
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-import models 
-from Item.itemBase import ItemBase 
+import models
+from Item.itemBase import ItemBase
 from DataBase import engine, SessionLocal
+from PIL import Image
+from keys import link
 
 router = APIRouter(
     prefix='/item',
     tags=['item']
 )
+
 
 def get_db():
     db = SessionLocal()
@@ -20,18 +25,60 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @router.post('/create/', status_code=status.HTTP_201_CREATED)
-async def create_Item(item: ItemBase, db: db_dependency):
-    db_Item = models.Item(**item.dict())
+async def create_Item(db: Session = Depends(get_db), Nome: str = Form(...), Descricao: str = Form(...), Categoria: str = Form(...), Preco: float = Form(...), Ativo: bool = Form(...), image: UploadFile = File(...)):
+    db_Item = models.Item(
+        Nome=Nome,
+        Descricao=Descricao,
+        Categoria=Categoria,
+        Preco=Preco,
+        Ativo=Ativo
+    )
+
+    try:
+        file = Image.open(image.file)
+
+    except Exception as err:
+        raise HTTPException(status_code=406, detail="The image file is not valid")
+
     db.add(db_Item)
     db.commit()
+    db.refresh(db_Item)
+
+    filePath = "imagens/" + str(db_Item.ID) + ".png"
+    file.save(filePath)
+
+    return "Item saved with success"
+
+
+@router.get("/getImage/{item}", status_code=status.HTTP_201_CREATED)
+async def get_image(item: int):
+    image_path = "imagens/" + str(item) + ".png"
+    return FileResponse(image_path, media_type="image/png")
 
 
 @router.get("/{item_id}", status_code=status.HTTP_200_OK)
 async def read_item(item_id: int, db: db_dependency):
-    user = db.query(models.Item).filter(models.Item.id == item_id).first()
-    if user is None:
+    item = db.query(models.Item).filter(models.Item.ID == item_id).first()
+
+    if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return user
+
+    URL = link + f"item/getImage/{item_id}"
+
+    Response = {
+        "item": {
+            "ID": item.ID,
+            "Descricao": item.Descricao,
+            "Categoria": item.Categoria,
+            "Preco": item.Preco,
+            "Ativo": item.Ativo
+        },
+        "imageURL": URL
+    }
+
+    return Response
