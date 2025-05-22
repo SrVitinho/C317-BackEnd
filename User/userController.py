256386ad7b5f2f66e1cc0ee92a3b87b19ab38fb6
SrVitinho <55586232+SrVitinho@users.noreply.github.com>
2025-05-22 12:56:17 -0300

    Add: New nedpoint for better user control

--- User/userController.py
+++ User/userController.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
 from typing import Annotated
 from sqlalchemy.orm import Session
-from User.userBase import UserBase, UserResponse
+from User.userBase import UserBase, UserResponse, UserUpdate
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from DataBase import engine, SessionLocal
 from models import *
+from typing import List
+
 
 router = APIRouter(
     prefix='/users',
@@ -35,11 +37,38 @@ async def create_user(user: UserBase, db: db_dependency):
     )
     db.add(db_user)
     db.commit()
+    return db_user
 
+@router.get("/all", response_model=List[UserResponse], status_code=status.HTTP_200_OK)
+async def get_all_users(db: db_dependency):
+    user = db.query(User).filter(User.Ativo == True).all()
+    if len(user) == 0:
+        raise HTTPException(status_code=404, detail="No User found in DB")
+    return user
 
-@router.get("/users/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
-async def read_user(user_id: int, db: db_dependency):
-    user = db.query(User).filter(User.id == user_id).first()
+@router.get("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+def read_user(user_id: int, db: db_dependency):
+    user = db.query(User).filter(User.ID == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+@router.put("/update/", status_code=status.HTTP_202_ACCEPTED)
+async def update_user(userUPT: UserUpdate, db: db_dependency):
+    
+    user = read_user(userUPT.ID, db)
+
+    user.userName = userUPT.userName
+    user.NumCel = userUPT.NumCel
+
+    db.add(user)
+    db.commit()
+
+@router.put("/Set/Role", status_code=status.HTTP_202_ACCEPTED)
+async def update_role(role: str, id_user: int, db: db_dependency):
+    user = read_user(id_user, db)
+    
+    user.role = role
+    
+    db.add(user)
+    db.commit()  
