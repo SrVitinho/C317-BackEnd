bcc9fd40b4377afca4a9d3098709ebf773369afe
SrVitinho <55586232+SrVitinho@users.noreply.github.com>
2025-05-27 13:09:03 -0300

    Add: Set status for pedido

--- Pedido/pedidoController.py
+++ Pedido/pedidoController.py
@@ -51,7 +51,21 @@ def get_Price(itens: list[ItemAdd], db: db_dependency):
 
 @router.put('/set/status', status_code=status.HTTP_200_OK)
 def set_Status(Status: str, id: int, db: db_dependency):
-    return "work in pogress"
+    pedido = db.query(models.Pedido).filter(models.Pedido.ID == id).first()
+
+    if pedido.ID is None:
+        raise HTTPException(status_code=403, detail="invalid pedido")
+
+    statusList = ["Orcado", "Pendente", "Aprovado", "Reprovado", "Pagamento", "Concluido"]
+
+    if Status not in statusList:
+        raise HTTPException(status_code=403, detail="Invalid Status")
+    
+    pedido.Status = Status
+
+    db.add(pedido)
+    db.commit()
+    return "update successful"
 
 
 @router.post('/create/', status_code=status.HTTP_201_CREATED, response_model=PedidoResponse)
